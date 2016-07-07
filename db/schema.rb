242eb02980f4a4d74cdf80b694a2449ccd300a44
Ward Verhoef <bypandad@gmail.com>
2016-07-07 15:31:24 +0200

    did migration to project so guard tests would pass

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160707090940) do
+ActiveRecord::Schema.define(version: 20160707130930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "characters", force: :cascade do |t|
+    t.string   "name"
+    t.string   "gender"
+    t.decimal  "money"
+    t.integer  "fighting"
+    t.integer  "trickery"
+    t.integer  "action"
+    t.integer  "talking"
+    t.integer  "perception"
+    t.integer  "innerstrength"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "choices", force: :cascade do |t|
     t.integer  "quest_id"
