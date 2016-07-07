e8404dd2a2c87765ab545c2cd3714a2cd31f1908
Ward Verhoef <bypandad@gmail.com>
2016-07-07 17:07:46 +0200

    add validation to Character model add 3 tests to character_spec.rb file that tests relation to user and validation of name and gender

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,23 @@
 class Character < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :name, :gender
+  # validates_presence_of  
+  #
+  # attr_points = 6
+  #
+  # fighting_attribute = 0
+  # trickery_attribute = 0
+  # action_attribute = 0
+  # talking_attribute = 0
+  # fighting_attribute = 0
+  #
+  #   if
+  #
+  #
+  #     t.integer  "fighting"
+  #     t.integer  "trickery"
+  #     t.integer  "action"
+  #     t.integer  "talking"
+  #     t.integer  "perception"
+  #     t.integer  "innerstrength"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160707144503) do
+ActiveRecord::Schema.define(version: 20160707150304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,10 @@ ActiveRecord::Schema.define(version: 20160707144503) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "attr_points"
+    t.integer  "user_id"
   end
+
+  add_index "characters", ["user_id"], name: "index_characters_on_user_id", using: :btree
 
   create_table "choices", force: :cascade do |t|
     t.integer  "quest_id"
@@ -68,5 +71,6 @@ ActiveRecord::Schema.define(version: 20160707144503) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "characters", "users"
   add_foreign_key "choices", "quests"
 end
