d2bbf7ef493b611cf11ebd680ebed5ca51d1dfe5
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-07-07 14:57:58 +0200

    Remove character from bag migration

--- db/migrate/20160707124553_create_bags.rb
+++ db/migrate/20160707124553_create_bags.rb
@@ -1,8 +1,6 @@
 class CreateBags < ActiveRecord::Migration
   def change
     create_table :bags do |t|
-      t.references :character, index: true, foreign_key: true
-
       t.timestamps null: false
     end
   end
--- db/migrate/20160707124821_create_items.rb
+++ db/migrate/20160707124821_create_items.rb
@@ -3,6 +3,7 @@ class CreateItems < ActiveRecord::Migration
     create_table :items do |t|
       t.string :name
       t.string :description
+      t.string :image
       t.references :bag, index: true, foreign_key: true
 
       t.timestamps null: false
