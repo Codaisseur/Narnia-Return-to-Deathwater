7a5c9a1aa66550b29022875d8708edca1dc2767a
Wouter de Vos <wouter@codaisseur.com>
2016-07-07 10:44:40 +0200

    Load all story seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Dir[Rails.root.join('db/stories/**/*.rb')].each { |f| require f }
