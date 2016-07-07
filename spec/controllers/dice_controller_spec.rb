274a1bfe87b7f42d3b84e2399d662d2250c33168
David Kumru <davidkumru@gmail.com>
2016-07-07 17:13:09 +0200

    fix spec filename

--- spec/controllers/dice_controller_spec.rb
+++ spec/controllers/dice_controller_spec.rb
@@ -1,29 +1,29 @@
 require 'rails_helper'
 
-describe ApplicationController do
+describe DiceController do
 
   describe "POST #roll" do
     context "with valid params" do
 
       it "receives three integer parameters" do
-        total = ApplicationController.new.roll(2, 12, 2)
+        total = DiceController.new.roll(2, 12, 2)
         expect(total).to be_a(Integer)
       end
 
       it "returns 12 if total is bigger than 12" do
-        total = ApplicationController.new.roll(2, 12, 9)
+        total = DiceController.new.roll(2, 12, 9)
         expect(total).to be_a(Integer)
         expect(2..12).to cover(total)
       end
 
       it "returns 2 if total is less than 2" do
-        total = ApplicationController.new.roll(2, 12, -9)
+        total = DiceController.new.roll(2, 12, -9)
         expect(total).to be_a(Integer)
         expect(2..12).to cover(total)
       end
 
       it "returns total if total is from 2 to 12" do
-        total = ApplicationController.new.roll(2, 12, 2)
+        total = DiceController.new.roll(2, 12, 2)
         expect(total).to be_a(Integer)
         expect(2..12).to cover(total)
       end
@@ -32,24 +32,24 @@ describe ApplicationController do
     context "with invalid params" do
 
       it "receives more or less than three integer parameters" do
-        total = ApplicationController.new.roll(2, 12, 2)
+        total = DiceController.new.roll(2, 12, 2)
         expect(total).to be_a(Integer)
       end
 
       it "doesn't return 12 if total is bigger than 12" do
-        total = ApplicationController.new.roll(2, 12, 9)
+        total = DiceController.new.roll(2, 12, 9)
         expect(total).to be_a(Integer)
         expect(2..12).to cover(total)
       end
 
       it "doesn't return 2 if total is less than 2" do
-        total = ApplicationController.new.roll(2, 12, -9)
+        total = DiceController.new.roll(2, 12, -9)
         expect(total).to be_a(Integer)
         expect(2..12).to cover(total)
       end
 
       it "doesn't return total if total is from 2 to 12" do
-        total = ApplicationController.new.roll(2, 12, 2)
+        total = DiceController.new.roll(2, 12, 2)
         expect(total).to be_a(Integer)
         expect(2..12).to cover(total)
       end
