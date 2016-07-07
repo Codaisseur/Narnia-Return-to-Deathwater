2d7c58dbfa56d5cf903ee2f32419ec30bda2e679
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-07-07 16:24:45 +0200

    Add new items to item-seed

--- db/items/items-seed.rb
+++ db/items/items-seed.rb
@@ -4,3 +4,6 @@ wishbone = Item.create([{ name: 'Wishbone' }, { description: 'Just a bone.'     
 marble   = Item.create([{ name: 'Marble'   }, { description: 'Much shiny, very round.' }, { image: 'app/assets/images/marble.jpg'   }, {bag: bag}])
 pennies  = Item.create([{ name: 'Pennies'  }, { description: 'A whopping 4 of them.'   }, { image:  'app/assets/images/4pen.jpg'    }, {bag: bag}])
 shilling = Item.create([{ name: 'Shilling' }, { description: 'Worth 5 pennies.'        }, { image: 'app/assets/images/shilling.jpg' }, {bag: bag}])
+weapon1  = Item.create([{ name: 'Excalibur' }, { description: 'The legendary sword of King Arthur' }, { image: 'app/assets/images/Sword1.jpg' }, {bag: bag}])
+helm1    = Item.create([{ name: 'Helm of the Divine War' }, { description: 'Worn by the forgotten king' }, { image: 'app/assets/images/helm1.jpg' }, {bag: bag}])
+chest1   = Item.create([{ name: 'Greatplate of Burning Wars' }, { description: 'Worn by royalty' }, { image: 'app/assets/images/chest1.jpeg' }, {bag: bag}])
