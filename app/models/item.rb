f96275df63b32f6f476a23393b2d6e926c70a2a7
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-07-07 15:43:42 +0200

    Add specs for item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :bag
+  validates_presence_of :name
+  validates_presence_of :bag_id
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Item, :type => :model do
+
+  describe "validations" do
+    it "is valid with a name" do
+      expect(Item.new(bag_id: 1, name: 'Anything')).to be_valid
+    end
+
+    it "is valid with a description" do
+      expect(Item.new(bag_id: 2, name: 'whatever', description: 'Anything')).to be_valid
+    end
+
+    it "is valid with an image" do
+      expect(Item.new(bag_id: 3, name: 'whatever', image: 'Anything')).to be_valid
+    end
+
+  end
+
+  it { is_expected.to belong_to :bag }
+  it { is_expected.to validate_presence_of :name}
+  it { is_expected.to validate_presence_of :bag_id}
+
 end
