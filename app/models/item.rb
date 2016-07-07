69efa9cd7c7d12d556925d723e10c1f2eafbfa62
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-07-07 16:59:37 +0200

    Fix specs and seed file

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,4 @@
 class Item < ActiveRecord::Base
   belongs_to :bag
-  validates_presence_of :name
-  validates_presence_of :bag_id
+  validates_presence_of :name, :bag_id
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,23 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Item, :type => :model do
 
-  describe "validations" do
-    it "is valid with a name" do
-      expect(Item.new(bag_id: 1, name: 'Anything')).to be_valid
-    end
-
-    it "is valid with a description" do
-      expect(Item.new(bag_id: 2, name: 'whatever', description: 'Anything')).to be_valid
-    end
-
-    it "is valid with an image" do
-      expect(Item.new(bag_id: 3, name: 'whatever', image: 'Anything')).to be_valid
-    end
-
-  end
-
   it { is_expected.to belong_to :bag }
-  it { is_expected.to validate_presence_of :name}
-  it { is_expected.to validate_presence_of :bag_id}
+
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :bag_id }
 
 end
