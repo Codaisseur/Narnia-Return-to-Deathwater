c7fcf1292e7cbbaef2475f4e370e9a73bfac8d25
Wouter de Vos <wouter@codaisseur.com>
2016-07-07 18:01:24 +0200

    Example specs for dice roller

--- app/services/dice_roller.rb
+++ app/services/dice_roller.rb
@@ -1,4 +1,6 @@
 class DiceRoller
+  attr_reader :min, :max, :bonus
+
   def initialize(options = {})
     @min = options.fetch(:min, 2)
     @max = options.fetch(:max, 12)
@@ -6,16 +8,17 @@ class DiceRoller
   end
 
   def roll
-    random_number = random.uniform(@min, @max).to_i
     [random_number + @bonus, @max].min
   end
 
+  def random_number
+    random.uniform(@min, @max).to_i
+  end
+
   def random
-    @random ||= begin
-      r = SimpleRandom.new
-      r.set_seed
-      r
-    end
+    r = SimpleRandom.new
+    r.set_seed
+    r
   end
 
   def to_json
--- spec/services/dice_roller_spec.rb
+++ spec/services/dice_roller_spec.rb
@@ -1,4 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe DiceRoller do
+  let(:roller) { DiceRoller.new }
+
+  describe "initialize" do
+    describe "default params" do
+
+      it "should set the defaults" do
+        expect(roller.min).to eq 2
+        expect(roller.max).to eq 12
+        expect(roller.bonus).to eq 0
+      end
+    end
+
+    describe "custom params" do
+      let(:roller) { DiceRoller.new(custom_params) }
+      let(:custom_params) { { min: 5, max: 24, bonus: 3 } }
+
+      it "should set min, max, and bonus accordingly" do
+        expect(roller.min).to eq custom_params[:min]
+        expect(roller.max).to eq custom_params[:max]
+        expect(roller.bonus).to eq custom_params[:bonus]
+      end
+    end
+
+    describe "#roll" do
+      let(:roller) { DiceRoller.new(custom_params) }
+      let(:custom_params) { { min: 5, max: 12, bonus: 3 } }
+
+      it "should never return a lower value than min" do
+        allow(roller).to receive(:random_number).and_return(0)
+
+        expect(roller.roll).to eq 5
+      end
+
+      it "should never return a higher value than max" do
+        allow(roller).to receive(:random_number).and_return(12)
+
+        expect(roller.roll).to eq 12
+      end
+    end
+  end
 end
