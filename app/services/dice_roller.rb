3d8009d4eb2b5e1c571d749d20566c4fa581f4f3
David Kumru <davidkumru@gmail.com>
2016-07-07 17:44:13 +0200

    WIP roller specs

--- app/services/dice_roller.rb
+++ app/services/dice_roller.rb
@@ -1,3 +1,24 @@
-class DiceRoller < ActiveRecord::Base
+class DiceRoller
+  def initialize(options = {})
+    @min = options.fetch(:min, 2)
+    @max = options.fetch(:max, 12)
+    @bonus = options.fetch(:bonus, 0)
+  end
 
+  def roll
+    random_number = random.uniform(@min, @max).to_i
+    [random_number + @bonus, @max].min
+  end
+
+  def random
+    @random ||= begin
+      r = SimpleRandom.new
+      r.set_seed
+      r
+    end
+  end
+
+  def to_json
+    { result: roll }
+  end
 end
