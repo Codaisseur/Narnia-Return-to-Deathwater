61a4b85ef221879c348af01d89c15b44791871ea
David Kumru <davidkumru@gmail.com>
2016-07-08 13:37:37 +0200

    pulled from origin

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,29 +1,12 @@
 class DiceController < ApplicationController
 
   def create
-    @diceroll = DiceRoller.new(dice_params)
-
-    r = SimpleRandom.new
-    r.set_seed
-    random_number = r.uniform(min, max).to_i
-    # random_number = rand(min.to_i..max.to_i)
-    puts "Random number: #{random_number}"
-    puts "Bonus: #{bonus}"
-    total = random_number + bonus
-    if total > 12
-      total = 12
-      puts "Total: #{random_number} + #{bonus} = #{total}"
-      puts "Total is higher than 12."
-      return total
-    elsif total < 2
-      puts "Total is lower than 2."
-      total = 2
-      puts "Total: #{random_number} + #{bonus} = #{total}"
-      return total
-    else
-      puts "Total: #{random_number} + #{bonus} = #{total}"
-      return total
-    end
+    render json: DiceRoller.new(dice_params)
   end
 
+  private
+
+  def dice_params
+    params.permit(:min, :max, :bonus)
+  end
 end
