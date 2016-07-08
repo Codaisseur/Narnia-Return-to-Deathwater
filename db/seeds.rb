c829fc739d35fcf59e447ff2c7d9da54d0c35434
Rosiene <rosiene.dilly@gmail.com>
2016-07-08 14:41:34 +0200

    add key and questKey models and seeds files

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Quest.destroy_all
+Key.destroy_all
+QuestKey.destroy_all
 
 Dir[Rails.root.join('db/stories/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('db/keys/key.rb')].each { |f| require f }
+Dir[Rails.root.join('db/keys/quest_key.rb')].each { |f| require f }
 
-puts "Seeded #{Quest.count} Quests and #{Choice.count} Choices!"
+puts "Seeded #{Quest.count} Quests, #{Choice.count} Choices, #{Key.count} Keys and #{QuestKey.count} QuestChoiceKey!"
