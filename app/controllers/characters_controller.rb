9c099839b8ae7a7e92bd2e1ab7d0a1b169af4c06
Ward Verhoef <bypandad@gmail.com>
2016-07-11 14:32:02 +0200

    changed char model to render chars only when signed in

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,10 +1,15 @@
 class CharactersController < ApplicationController
 
   def index
-    @characters = Character.all
+    @characters = current_user.characters
     render json: @characters
   end
 
+  def create
+
+  end
+
   def updated
-  end 
+  end
+
 end
