7f94bdf47ad47730a8d1aaf4a232fdacc839769c
dtolhuijs <deisi.bonilla@gmail.com>
2016-07-11 11:06:51 +0200

    create test for key

--- spec/models/key_spec.rb
+++ spec/models/key_spec.rb
@@ -3,3 +3,10 @@ require 'rails_helper'
 RSpec.describe Key, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe "#create" do
+    it "creates a key" do
+      @key = Key.create(number: 10)
+      @key.should be_an_instance_of Key
+    end
+  end
