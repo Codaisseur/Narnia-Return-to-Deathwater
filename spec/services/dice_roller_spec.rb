5a1bb577ad6f17615f4aa1cad0a8c825036863eb
David Kumru <davidkumru@gmail.com>
2016-07-11 16:33:33 +0200

    added tests

--- spec/services/dice_roller_spec.rb
+++ spec/services/dice_roller_spec.rb
@@ -1,44 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe DiceRoller do
-  let(:roller) { DiceRoller.new(3) }
-
   describe "initialize" do
-    describe "default params and bonus" do
+    describe "default params and bonus argument" do
+      let(:roller) { DiceRoller.new(3) }
 
-      it "should set the default min and max and accept the provided bonus" do
+      it "should set the default min and max and accept the passed bonus argument" do
         expect(roller.min).to eq 2
         expect(roller.max).to eq 12
         expect(roller.bonus).to eq(3)
       end
     end
 
-    # test per function
+    describe "default params and bonus argument" do
+      let(:roller) { DiceRoller.new("3") }
 
-    # describe "custom params" do
-    #   let(:roller) { DiceRoller.new(3) }
-    #
-    #   it "should accept the provided bonus" do
-    #     expect(roller.min).to eq(2)
-    #     expect(roller.max).to eq(12)
-    #   end
-    # end
+      it "should process the numerical string as an integer" do
+        expect(roller.min).to eq 2
+        expect(roller.max).to eq 12
+        expect(roller.bonus).to eq(3)
+      end
+    end
 
-    # describe "#roll" do
-    #   let(:roller) { DiceRoller.new(custom_params) }
-    #   let(:custom_params) { { min: 5, max: 12, bonus: 3 } }
-    #
-    #   it "should never return a lower value than min" do
-    #     allow(roller).to receive(:random_number).and_return(0)
-    #
-    #     expect(roller.roll).to eq 5
-    #   end
-    #
-    #   it "should never return a higher value than max" do
-    #     allow(roller).to receive(:random_number).and_return(12)
-    #
-    #     expect(roller.roll).to eq 12
-    #   end
-    # end
+    describe "low number" do
+      let(:roller) { DiceRoller.new(-20) }
+
+      it "should never return a lower value than min" do
+        expect(roller.roll).to eq 2
+      end
+    end
+
+    describe "high number" do
+      let(:roller) { DiceRoller.new(20) }
+
+      it "should never return a higher value than max" do
+        expect(roller.roll).to eq 12
+      end
+    end
+
+    describe "non-numerical string" do
+      let(:roller) { DiceRoller.new("text") }
+
+      it "should return error when receiving a non-numerical string" do
+        expect { roller.roll }.to raise_error(ArgumentError)
+      end
+    end
+
+    describe "object" do
+      let(:roller) { DiceRoller.new(text) }
+
+      it "should return error when not receiving an integer or string" do
+        expect { roller.roll }.to raise_error(NameError)
+      end
+    end
   end
 end
