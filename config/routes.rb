13a0317a4bb8edaecb559421f671c14f9f00b56b
Remeeus <remeeus@hotmail.com>
2016-07-11 12:27:56 +0200

    Outcommented the specs for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :items
-  resource  :bag
+  resource :bag
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
